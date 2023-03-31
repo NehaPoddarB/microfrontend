c9265d1534bdc6963176c6f983170be42156a7f9
Anuj Tomar <anuj.tomar@knoldus.com>
2023-03-31 19:13:49 +0530

    No data found message

--- company-management/src/components/table/StickyTable.js
+++ company-management/src/components/table/StickyTable.js
@@ -7,12 +7,12 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Typography, Container } from '@mui/material';
 import { useTheme, useMediaQuery } from '@material-ui/core';
 import TableCards from '../TableCard/Tablecards';
 // import WebButton from 'button/Button-web-component';
 import '../../../../indexOne';
-
+import ErrorRoundedIcon from '@mui/icons-material/ErrorRounded';
 
 export default function StickyTable({ columns, rows, label, handleOpenAdd, tableName }) {
   const [page, setPage] = React.useState(0);
@@ -65,63 +65,79 @@ export default function StickyTable({ columns, rows, label, handleOpenAdd, table
         marginRight: "2rem",
         marginTop:'-1rem'
       }}>
-        <TableContainer sx={{
-          width: "max-content",
-          marginLeft: "auto",
-          marginRight: "auto",
-          marginTop: 4,
-          borderRadius: 2,
-          width: "100%",
-          overflowX: "auto",
-          marginBottom:'4rem',
-        }}>
-          {showText ? <Table stickyHeader aria-label="sticky table" sx={{ padding: '17px' }}>
-            <TableHead >
-              <TableRow sx={{
-                "& th": {
-                  fontSize: "1rem",
-                  fontWeight: '700',
-                  border: " 1 solid rgba(0, 0, 0, 0.125)",
-                  // color: "#b1acacd1",
-                  textTransform: 'uppercase',
-                  opacity: 1
-                },
-              }}>
-                {columns.map((column) => (
-                  <TableCell
-                    key={column.id}
-                    align={column.align}
-                    style={{ minWidth: column.minWidth, fontSize: "0.875rem" }}
-                  >
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => {
-                  return (
-                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                      {columns.map((column) => {
-                        const value = row[column.id];
-                        return (
-                          <TableCell key={column.id} align={column.align} sx={{ fontSize: '1rem' }} >
-                            {column.format && typeof value === 'number'
-                              ? column.format(value)
-                              : value}
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
-          </Table>:  (rows.map((row) => (
-          <TableCards rows={row} columns={columns}/>
-        )))}
-        </TableContainer>
+          <TableContainer sx={{
+            width: "max-content",
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginTop: 4,
+            borderRadius: 2,
+            width: "100%",
+            overflowX: "auto",
+            marginBottom: '4rem',
+          }}>
+            {showText ? <Table stickyHeader aria-label="sticky table" sx={{ px: '17px', pt: '17px' }}>
+              <TableHead >
+                <TableRow sx={{
+                  "& th": {
+                    fontSize: "1rem",
+                    fontWeight: '700',
+                    border: " 1 solid rgba(0, 0, 0, 0.125)",
+                    // color: "#b1acacd1",
+                    textTransform: 'uppercase',
+                    opacity: 1
+                  },
+                }}>
+                  {columns.map((column) => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth, fontSize: "0.875rem" }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              {rows.length > 0 ?
+              <TableBody>
+                {rows
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => {
+                    return (
+                      <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                        {columns.map((column) => {
+                          const value = row[column.id];
+                          return (
+                            <TableCell key={column.id} align={column.align} sx={{ fontSize: '1rem' }} >
+                              {column.format && typeof value === 'number'
+                                ? column.format(value)
+                                : value}
+                            </TableCell>
+                          );
+                        })}
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>: 
+               <Container maxWidth="xs"> <Box sx={{ display: 'flex', marginTop: '1rem' }}>
+               <ErrorRoundedIcon fontSize="medium" color="error" sx={{  marginRight: 1 }} />
+               <Typography
+                 variant="h6"
+                 fontWeight="400"
+                 mb={0.5}
+                 color="error"
+                 alignItems="left"
+                 alignContent="left"
+                 sx={{  width: "100%" }}
+               >
+                 No {tableName} Found
+               </Typography>
+             </Box>
+             </Container>}
+            </Table> : (rows.map((row) => (
+              <TableCards key={row.code} rows={row} columns={columns} />
+            )))}
+          </TableContainer>
       </Paper>
     </>
   );
