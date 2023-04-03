58b69a3c7cf5e81b39a46ac0d28223769cee7a20
Anuj Tomar <anuj.tomar@knoldus.com>
2023-04-03 23:00:01 +0530

    Fixed No data message

--- company-management/src/components/table/StickyTable.js
+++ company-management/src/components/table/StickyTable.js
@@ -18,9 +18,15 @@ import ErrorRoundedIcon from '@mui/icons-material/ErrorRounded';
 export default function StickyTable({ columns, rows, label, tableName, handleOpenAdd }) {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [noDataText, setNoDataText] = React.useState(false);
   const theme = useTheme();
   const showText = useMediaQuery(theme.breakpoints.up('md'));
 
+  React.useEffect(()=>{
+    if(rows.length === 0){
+      setNoDataText(true)
+    }
+  },[rows])
   return (
     <>
       <Box sx={{
@@ -67,40 +73,39 @@ export default function StickyTable({ columns, rows, label, tableName, handleOpe
         marginRight: "2rem",
         marginTop: '-1rem'
       }}>
-          <TableContainer sx={{
-            width: "max-content",
-            marginLeft: "auto",
-            marginRight: "auto",
-            marginTop: 4,
-            borderRadius: 2,
-            width: "100%",
-            overflowX: "auto",
-            marginBottom: '4rem',
-          }}>
-            {showText ? <Table stickyHeader aria-label="sticky table" sx={{ px: '17px', pt: '17px' }}>
-              <TableHead >
-                <TableRow sx={{
-                  "& th": {
-                    fontSize: "1rem",
-                    fontWeight: '700',
-                    border: " 1 solid rgba(0, 0, 0, 0.125)",
-                    // color: "#b1acacd1",
-                    textTransform: 'uppercase',
-                    opacity: 1
-                  },
-                }}>
-                  {columns.map((column) => (
-                    <TableCell
-                      key={column.id}
-                      align={column.align}
-                      style={{ minWidth: column.minWidth, fontSize: "0.875rem" }}
-                    >
-                      {column.label}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              {rows.length > 0 ?
+        <TableContainer sx={{
+          width: "max-content",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: 4,
+          borderRadius: 2,
+          width: "100%",
+          overflowX: "auto",
+        }}>
+          {showText ? <Table stickyHeader aria-label="sticky table" sx={{ px: '17px', pt: '17px' }}>
+            <TableHead >
+              <TableRow sx={{
+                "& th": {
+                  fontSize: "1rem",
+                  fontWeight: '700',
+                  border: " 1 solid rgba(0, 0, 0, 0.125)",
+                  // color: "#b1acacd1",
+                  textTransform: 'uppercase',
+                  opacity: 1
+                },
+              }}>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth, fontSize: "0.875rem" }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            {rows.length > 0 &&
               <TableBody>
                 {rows
                   .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
@@ -120,26 +125,27 @@ export default function StickyTable({ columns, rows, label, tableName, handleOpe
                       </TableRow>
                     );
                   })}
-              </TableBody>: 
-               <Container maxWidth="xs"> <Box sx={{ display: 'flex', marginTop: '1rem' }}>
-               <ErrorRoundedIcon fontSize="medium" color="error" sx={{  marginRight: 1 }} />
-               <Typography
-                 variant="h6"
-                 fontWeight="400"
-                 mb={0.5}
-                 color="error"
-                 alignItems="left"
-                 alignContent="left"
-                 sx={{  width: "100%" }}
-               >
-                 No {tableName} Found
-               </Typography>
-             </Box>
-             </Container>}
-            </Table> : (rows.map((row) => (
-              <TableCards key={row.code} rows={row} columns={columns} />
-            )))}
-          </TableContainer>
+              </TableBody> 
+            }
+          </Table> : (rows.map((row) => (
+            <TableCards key={row.code} rows={row} columns={columns} />
+          )))}
+        </TableContainer>
+        {noDataText &&   <Container maxWidth="xs"> <Box sx={{ display: 'flex', marginTop: '1rem' }}>
+                 <ErrorRoundedIcon fontSize="medium" color="error" sx={{  marginRight: 1 }} />
+                 <Typography
+                   variant="h6"
+                   fontWeight="400"
+                   mb={0.5}
+                   color="error"
+                   alignItems="left"
+                   alignContent="left"
+                   sx={{  width: "100%" }}
+                 >
+                   No {tableName} Found
+                 </Typography>
+               </Box>
+               </Container>}
       </Paper>
     </>
   );
